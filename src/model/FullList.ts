c97ec52133f72ef804a703179bd13fc7f981e600
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-11-24 09:50:16 +0900

    adding Fulllists
    Added the main full-list add

--- src/model/FullList.ts
+++ src/model/FullList.ts
@@ -11,5 +11,31 @@ interface List {
 }
 
 export default class FullList implements List {
-    
-}
+
+    static instance: FullList = new FullList()
+
+    private constructor(private _list: ListITem[] = []){}
+
+    get List(): ListItem[] {
+        return this._list
+    }
+
+    save(): void {
+        localStorage.setItem("myList", JSON.stringify(this._list))
+    }
+
+    clearList(): void {
+        this._list = []
+        this.save()
+    }
+
+    addITem(itemObj: ListItem): void {
+        this._list.push(itemObj)
+        this.save()
+    }
+
+    removeItem(id: string): void {
+        this._list = this._list.filter(item => item.id !== id)
+        this.save()
+    }
+}   
