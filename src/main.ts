ed9184b00ae127cbb9b4afd48086cb2a590bfaee
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-11-25 13:00:04 +0900

    ClearList
    Added the main clear list for the code.

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,24 @@
 import './css/style.css';
+import FullList from './model/FullList';
+import ListItem from './model/ListItem';
+import ListTempate from './templates/ListTemplate';
 
+const initApp = (): void => { 
+    const fullList = FullList.instance
+    const templates = ListTempate.instance
+
+    const itemEntryForm = document.getElementById("itemEntryForm") as HTMLFormElement
+    itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+        event.preventDefault()
+
+    })
+
+
+    const clearItems = document.getElementById("clearItemsButton") as HTMLButtonElement
+
+    clearItems.addEventListener('click', (): void => {
+        fullList.clearList()
+    })
+}
+
+document.addEventListener("DOMContentLoaded", initApp) 
